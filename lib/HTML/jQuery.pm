c261714d0516083251b447099063a090dac3414f
Andy Armstrong <andy@hexten.net>
2011-07-10 01:30:12 +0100

    Snapshot

--- lib/HTML/jQuery.pm
+++ lib/HTML/jQuery.pm
@@ -73,7 +73,7 @@ sub _wrap {
 sub jq {
   my ( $self, $sel ) = @_;
   $self = HTML::jQuery->new( $self )
-   unless blessed $self && $self->isa( 'HTML::jQuery' );
+   unless blessed $self && $self->isa( __PACKAGE__ );
   my $match = sub {
     my $nd = shift;
     return ( $nd ) if $nd->tagName eq 'A';
@@ -82,6 +82,86 @@ sub jq {
 
   return $self->_wrap(
     $self->_visitor_recursive( $self->{node}->firstChild, $match ) );
+}
+
+my $NAME = qr{[-\w]*};
+
+sub css_attr {
+  my $attr = shift;
+  return sub { exists $_[0]->{attr}{$attr} }
+   if $attr !~ m{=};
+  if ( $attr =~ m{^($NAME)="(.*?)"$} ) {
+    my ( $k, $v ) = ( $1, $2 );
+    return sub { exists $_[0]{attr}{$k} && $_[0]{attr}{$k} eq $v };
+  }
+  if ( $attr =~ m{^($NAME)~="(.*?)"$} ) {
+    my ( $k, $v ) = ( $1, $2 );
+    return sub {
+      return 0 unless defined $_[0]{attr}{$k};
+      return 1 if grep { $_ eq $v } split /\s+/, $_[0]{attr}{$k};
+      return 0;
+    };
+  }
+  die "Don't understand $attr";
+}
+
+sub css_term {
+  my $t = shift;
+
+  return sub { 1 }
+   if $t eq '*';
+  return sub { $_[0]->{tag} eq $t }
+   if $t =~ /^[-\w]+$/;
+
+  $t =~ s/^($NAME)#($NAME)$/${1}[id="${2}"]/g;
+  $t =~ s/^($NAME)\.($NAME)$/${1}[class~="${2}"]/g;
+
+  die "Unsupported syntax: $t" unless $t =~ m{^($NAME)\[(.+?)\]$};
+  my ( $tag, $attr ) = ( $1, $2 );
+  my $test = css_attr( $attr );
+  return $test unless length $tag;
+  return sub { $_[0]->{tag} eq $tag && $test->( $_[0] ) };
+}
+
+sub css_test {
+  my $t  = shift;
+  my $tt = css_term( $t );
+  return sub {
+    return 0 unless @_;
+    return $tt->( $_[0] );
+  };
+}
+
+sub css_selector {
+  my @p = split /\s+/, join ' ', @_;
+  return sub { 0 }
+   unless @p;
+
+  my $t = css_test( shift @p );
+  while ( @p ) {
+    my $lt = $t;
+    if ( $p[0] eq '>' ) {
+      shift @p;
+      my $nt = css_test( shift @p );
+      $t = sub {
+        my ( $nd, @tail ) = @_;
+        return $nt->( $nd ) && $lt->( @tail );
+      };
+    }
+    else {
+      my $nt = css_test( shift @p );
+      $t = sub {
+        my ( $nd, @tail ) = @_;
+        return 0 unless $nt->( $nd );
+        while ( @tail ) {
+          return 1 if $lt->( @tail );
+          shift @tail;
+        }
+        return 0;
+      };
+    }
+  }
+  return sub { $t->( reverse @_ ) ? 1 : 0 };
 }
 
 1;
